bfd9f24dcf7d3f43b1d878cee5822c9a2eeb93de
strjak <strackjuanp@gmail.com>
2026-01-05 18:15:34 +0000

    Implement basic drawing behaviour

--- main.js
+++ main.js
@@ -1,9 +1,45 @@
 const container = document.querySelector(".container");
 
-
-
-const defaultGridSize = 16;
+const defaultGridSize = 32;
 const defaultDivColor = "#ddd";
+
+let selectedColor = "#AA0033";
+
+drawGrid(defaultGridSize);
+
+// set event listener for squres in the grid
+document.querySelectorAll(".square").forEach(square => {
+    square.addEventListener("contextmenu", (e) => {
+        e.preventDefault()
+    });
+    square.addEventListener("mouseenter", () => {
+        printSquare(square, selectedColor);
+    });
+    square.addEventListener("mouseleave", () => {
+        if (!square.classList.contains("printed")){
+            printSquare(square, defaultDivColor);
+        }
+    });
+    square.addEventListener("mousemove", (e) => {
+        if (e.buttons === 1 && !square.classList.contains("printed")){
+            square.classList.add("printed");
+        }
+
+        if (e.buttons === 2 && square.classList.contains("printed")){
+            square.classList.remove("printed");
+        }
+    })
+    square.addEventListener("mousedown", (e) => {
+        if (e.buttons & 1){
+            printSquare(square, selectedColor);
+            square.classList.add("printed");
+        }else if (square.classList.contains("printed")){
+            printSquare(square, defaultDivColor);
+            square.classList.remove("printed");
+        }
+    });
+});
+
 
 function drawGrid(size){
     for (let row = 0; row < size; row++){
@@ -19,4 +55,7 @@ function drawGrid(size){
     }
 }
 
-drawGrid(defaultGridSize);
+/** @param {HTMLDivElement} square */
+function printSquare(square, color){
+    square.style.backgroundColor = color;
+}
